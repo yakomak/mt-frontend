71323ac587b59e06f6e8624f8ce42348f2cbc935
yakomak <yakomak137@gmail.com>
2018-01-23 18:01:52 +0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {}
+
+const mutations = {}
+
+const Store = new Vuex.Store({
+  state,
+  mutations
+})
+
+export default Store
